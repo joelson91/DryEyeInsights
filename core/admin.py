2380b745fb78d1c6d1084967054d43b06cf5e0c6
joelson <joelsonandradep@gmail.com>
2025-03-04 17:47:15 -0300

    update admin.py file to show models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Pesquisador, Participante
 
-# Register your models here.
+
+@admin.register(Pesquisador)
+class PesquisadorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'genero', 'email', 'telefone', 'instituicao', 'dt_cadastro')
+    list_filter = ('nome', 'genero', 'dt_cadastro')
+    search_fields = ('nome', 'genero', 'dt_cadastro')
+
+    def dt_cadastro(self, obj):
+        return obj.dt_cadastro
+
+    dt_cadastro.short_description = 'Data de Cadastro'
+
+
+@admin.register(Participante)
+class ParticipanteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'telefone')
+    list_filter = ('nome', 'email')
+    search_fields = ('nome', 'email')
