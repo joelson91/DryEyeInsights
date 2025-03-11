cc4a086b85ad9ba656793a9b80b32a3acbb6022c
joelson <joelsonandradep@gmail.com>
2025-03-11 19:31:17 -0300

    change QuestionarioForm fields

--- core/forms.py
+++ core/forms.py
@@ -7,7 +7,7 @@ class ParticipanteForm(forms.ModelForm):
         model = Participante
         fields = '__all__'
         widgets = {
-            'dt_nascimento': forms.DateInput(attrs={'type': 'date'}),
+            'dt_nascimento': forms.DateInput(attrs={'type': 'date'}, format='%Y-%m-%d'),
             'telefone': forms.TextInput(attrs={'type': 'tel'}),
         }
 
@@ -15,4 +15,29 @@ class ParticipanteForm(forms.ModelForm):
 class QuestionarioForm(forms.ModelForm):
     class Meta:
         model = Questionario
-        fields = '__all__'
+        fields = [
+            'duracao_sono',
+            'qualidade_sono',
+            'nivel_estresse',
+            'pressao_arterial',
+            'frequencia_cardiaca',
+            'passos_diarios',
+            'atividade_fisica',
+            'altura',
+            'peso',
+            'disturbio_sono',
+            'despertares_noturnos',
+            'sonolencia_diurna',
+            'consumo_cafeina',
+            'consumo_alcool',
+            'tabagismo',
+            'problema_medico',
+            'tomando_medicacao',
+            'uso_smartphone',
+            'tempo_medio_tela',
+            'filtro_luz_azul',
+            'fadiga_ocular',
+            'vermelhidao_ocular',
+            'irritacao_ocular',
+            'doenca_olho_seco',
+        ]
