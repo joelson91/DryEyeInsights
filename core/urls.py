66432dd4efae00c35f6937509573e7db468db79c
joelson <joelsonandradep@gmail.com>
2025-03-11 19:35:03 -0300

    add new routes to views.py

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,8 @@ from core import views
 urlpatterns = [
     path('', views.painel, name='painel'),
     path('participantes/', views.ver_participantes, name='ver_participantes'),
-    path('detalhes/<int:id>/', views.detalhe_participante, name='detalhes_participante'),
-    path('novo_participante/', views.criar_participante, name='criar_participante'),
-    path('questionario/', views.aplicar_questionario, name='aplicar_questionario'),
+    path('novo-participante/', views.criar_participante, name='criar_participante'),
+    path('detalhes/<int:id_participante>/', views.detalhe_participante, name='detalhes_participante'),
+    path('editar-participante/<int:id_participante>/', views.editar_participante, name='editar_participante'),
+    path('questionario/<int:id_participante>/', views.aplicar_questionario, name='aplicar_questionario'),
 ]
