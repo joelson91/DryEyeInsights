601c3361ed6422007fd7a7bf9d569ce942031fd1
joelson <joelsonandradep@gmail.com>
2025-03-12 13:12:37 -0300

    add new url to edit and exclude objects

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('novo-participante/', views.criar_participante, name='criar_participante'),
     path('detalhes/<int:id_participante>/', views.detalhe_participante, name='detalhes_participante'),
     path('editar-participante/<int:id_participante>/', views.editar_participante, name='editar_participante'),
+    path('excluir-participante<int:id_participante>', views.excluir_participante, name='excluir_participante'),
     path('questionario/<int:id_participante>/', views.aplicar_questionario, name='aplicar_questionario'),
     path('ver-questionario/<int:id_participante>/', views.ver_questionario, name='ver_questionario'),
+    path('editar_questionario<int:id_questionario>/', views.editar_questionario, name='editar_questionario'),
+    path('excluir-questionario<int:id_questionario>', views.excluir_questionario, name='excluir_questionario'),
 ]
