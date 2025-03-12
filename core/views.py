70749800bf7c88ae7071ede701d736bd81f5d636
joelson <joelsonandradep@gmail.com>
2025-03-12 12:06:35 -0300

    update views details

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
-from core.models import Participante, Pesquisador
+from core.models import Participante, Pesquisador, Questionario
 from core.forms import ParticipanteForm, QuestionarioForm
 
 
@@ -10,32 +10,45 @@ def index(request):
 
 @login_required
 def painel(request):
-    return render(request, 'painel/painel.html')
+    pesquisador = Pesquisador.objects.get(user=request.user)
+    return render(request, 'painel/painel.html', {'pesquisador': pesquisador})
 
 
 @login_required
 def criar_participante(request):
-    form = ParticipanteForm()
+    pesquisador = get_object_or_404(Pesquisador, user=request.user)
+
     if request.method == 'POST':
         form = ParticipanteForm(request.POST)
         if form.is_valid():
-            form.save()
+            questionario = form.save(commit=False)
+            questionario.id_pesquisador = pesquisador
+            questionario.save()
             return redirect('painel')
     else:
+        form = ParticipanteForm()
         return render(request, 'painel/registro_participante.html', {'form': form})
 
 
 @login_required
 def ver_participantes(request):
-    participantes = Participante.objects.all()
+    pesquisador_id = get_object_or_404(Pesquisador, user=request.user)
+    participantes = Participante.objects.filter(id_pesquisador=pesquisador_id)
     context = {'participantes': participantes}
     return render(request, 'painel/lista_participantes.html', context=context)
 
 
 @login_required
 def detalhe_participante(request, id_participante):
-    participante = get_object_or_404(Participante, pk=id_participante)
-    return render(request, 'painel/detalhe_participante.html', {'participante': participante})
+    participante = get_object_or_404(Participante, pk=id_participante, id_pesquisador__user=request.user)
+    questionario_preenchido = Questionario.objects.filter(id_participante=participante).exists()
+
+    context = {
+        'participante': participante,
+        'questionario_preenchido': questionario_preenchido,
+    }
+
+    return render(request, 'painel/detalhe_participante.html', context=context)
 
 
 @login_required
@@ -70,3 +83,9 @@ def aplicar_questionario(request, id_participante):
             return redirect(detalhe_participante, id_participante)
     else:
         return render(request, 'painel/questionario.html', {'form': form})
+
+
+@login_required
+def ver_questionario(request, id_participante):
+    questionario = get_object_or_404(Questionario, id_participante=id_participante)
+    return render(request, 'painel/ver_questionario.html', {'questionario': questionario})
